ed41641ee511c1c23dce23645aafe399c57dc16c
Tamjidul-Islam <tamjidtj6@gmail.com>
2025-11-05 18:55:43 +0600

    Added SOlution to Problem 383

---
+++ Solutions/383.ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       unordered_map<char,int>mpp;
+       for(auto ch:magazine) mpp[ch]++;
+       for(auto ch:ransomNote){
+        mpp[ch]--;
+        if(mpp[ch]<0) return false;
+       }
+       return true;
+    }
+};
